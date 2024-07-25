14d7568fbf45677a505421f53ad3b0df6bb338eb
Sharon Park <spark11270@gmail.com>
2024-07-25 09:15:50 -0400

    update main page

--- app/page.tsx
+++ app/page.tsx
@@ -22,20 +22,6 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <Image
-            src="/hero-desktop.png"
-            width={1000}
-            height={760}
-            className="hidden md:block"
-            alt="screenshots of the dashboard project showing desktop version"
-          />
-          <Image
-            src="/hero-mobile.png"
-            width={560}
-            height={620}
-            className="block md:hidden"
-            alt="screeshowts of the desktop project showing mobile version"
-          />
         </div>
       </div>
     </main>
